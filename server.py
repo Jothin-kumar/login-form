f96bcb08934b0275b7d8340838221355e98a29d6
Jothin kumar <bjothinphysics@gmail.com>
2021-11-30 09:21:27 +0530

    Create Flask app

--- server.py
+++ server.py
@@ -23,5 +23,41 @@ SOFTWARE.
 
 Author: Jothin kumar (https://jothin-kumar.github.io)
 """
+import flask
+from os.path import exists
 
-# Coming soon...
+app = flask.Flask(__name__)
+username_and_password_file = "username_and_passwords.txt"
+
+
+@app.route('/authenticate')
+def authenticate():
+    given_username = flask.request.args.get('username')
+    given_password = flask.request.args.get('password')
+    username_and_passwords = []
+    if exists(username_and_password_file):
+        with open(username_and_password_file) as usernames_and_passwords:
+            for line in usernames_and_passwords.readlines():
+                if line != '\n':
+                    username = line.split(' ')[0]
+                    password = line.split(' ')
+                    del password[0]
+                    password = ''.join(password).replace('\n', '')
+                    username_and_passwords.append({'username': username, 'password': password})
+    for username_and_password in username_and_passwords:
+        if username_and_password['username'] == given_username and username_and_password['password'] == given_password:
+            return 'Authenticated'
+    return 'Not Authenticated'
+
+
+@app.route('/signup')
+def signup():
+    username = flask.request.args.get('username')
+    password = flask.request.args.get('password')
+    with open(username_and_password_file, 'a+') as username_and_passwords:
+        username_and_passwords.write(username + ' ' + password + '\n')
+    return 'Successful'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
